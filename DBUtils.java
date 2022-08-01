199442c7b203449d9a17ab90161449ef143874a4
G Saibaba <saigaara@gmail.com>
2022-08-01 15:30:20 +0530

    first commit

---
+++ DBUtils.java
@@ -0,0 +1,6 @@
+hello guys
+what are you doing
+where are you going
+what can we are doing
+what is the next level of process
+what is the procedure
---
+++ Utils.java
@@ -0,0 +1,5 @@
+iam goin to school
+program schudle 
+school time teacher
+intermediate
+university
